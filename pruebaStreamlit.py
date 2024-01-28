186bba7fbbdc9eeef84345a987e7d73dbdb09a8b
Tirso Meneses Rojas <115514929+tmenesesr@users.noreply.github.com>
2024-01-27 21:52:40 -0300

    Update pruebaStreamlit.py

--- pruebaStreamlit.py
+++ pruebaStreamlit.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
-st.title("Uber pickups in NYC")
+st.title("Uber pickups in NYC by Óscar")
 
 DATE_COLUMN = "date/time"
 DATA_URL = (
